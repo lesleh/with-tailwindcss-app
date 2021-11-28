ee5e93336afd8c15ac31ed5eb22951a68e9fde97
Leslie Hoare <iam@lesleh.co.uk>
2021-11-28 12:30:49 +0000

    Add Storybook

---
+++ components/Button/Button.stories.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { Button } from "./Button";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/Button",
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args}>Button</Button>;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  variant: "primary",
+  label: "Button",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  variant: "secondary",
+  label: "Button",
+};
+
+export const GhostPrimary = Template.bind({});
+GhostPrimary.args = {
+  variant: "ghost-primary",
+  label: "Button",
+};
+
+export const GhostSecondary = Template.bind({});
+GhostSecondary.args = {
+  variant: "ghost-secondary",
+  label: "Button",
+};
