b31d7cefd16a4aa7d8303bcd075ed4e636fa1cc6
Leslie Hoare <iam@lesleh.co.uk>
2021-11-28 14:53:52 +0000

    Remove Storybook comments

--- components/Button/Button.stories.js
+++ components/Button/Button.stories.js
@@ -2,11 +2,9 @@ import React from "react";
 
 import { Button } from "./Button";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Atoms/Button",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     label: { control: "text" },
     variant: {
@@ -24,11 +22,9 @@ export default {
   },
 };
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   variant: "primary",
   label: "Button",
--- components/Heading/Heading.stories.js
+++ components/Heading/Heading.stories.js
@@ -2,11 +2,9 @@ import React from "react";
 
 import { Heading } from "./Heading";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Atoms/Heading",
   component: Heading,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     children: { control: "text" },
     element: {
@@ -18,46 +16,39 @@ export default {
   },
 };
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Heading {...args} />;
 
 export const Heading1 = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Heading1.args = {
   element: "h1",
   children: "Heading 1",
 };
 
 export const Heading2 = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Heading2.args = {
   element: "h2",
   children: "Heading 2",
 };
 
 export const Heading3 = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Heading3.args = {
   element: "h3",
   children: "Heading 3",
 };
 
 export const Heading4 = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Heading4.args = {
   element: "h4",
   children: "Heading 4",
 };
 
 export const Heading5 = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Heading5.args = {
   element: "h5",
   children: "Heading 5",
 };
 
 export const Heading6 = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Heading6.args = {
   element: "h6",
   children: "Heading 6",
