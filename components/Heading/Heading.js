1877c48652baa457b1f9121a19621c668d980418
Leslie Hoare <iam@lesleh.co.uk>
2021-11-28 13:07:02 +0000

    Reduce heading margins

--- components/Heading/Heading.js
+++ components/Heading/Heading.js
@@ -1,12 +1,12 @@
 import clsx from "clsx";
 
 const CLASSES = {
-  h1: "font-roboto uppercase text-5xl font-bold my-7",
-  h2: "font-roboto uppercase text-4xl font-bold my-6",
-  h3: "font-roboto uppercase text-3xl font-bold my-5",
-  h4: "font-roboto uppercase text-2xl font-bold my-4",
-  h5: "font-roboto uppercase text-xl font-bold my-3",
-  h6: "font-roboto uppercase text-lg font-bold my-2",
+  h1: "font-roboto uppercase text-5xl font-bold my-6",
+  h2: "font-roboto uppercase text-4xl font-bold my-5",
+  h3: "font-roboto uppercase text-3xl font-bold my-4",
+  h4: "font-roboto uppercase text-2xl font-bold my-3",
+  h5: "font-roboto uppercase text-xl font-bold my-2",
+  h6: "font-roboto uppercase text-lg font-bold my-1",
 };
 
 export function Heading({
